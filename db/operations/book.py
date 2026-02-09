286cdeaf8166b8e0998c47ecf655dee07130742d
cristian sáez <44517223+cristianS97@users.noreply.github.com>
2026-02-08 21:07:34 -0300

    Se retornan libros por orden decendente

--- db/operations/book.py
+++ db/operations/book.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 from models.models import Book
 from models.schemas import BookCreate
-from sqlalchemy import func
+from sqlalchemy import func, desc
 
 def create_book(db: Session, book: BookCreate, image_path: str = None):
     new_book = Book(title = book.title, author = book.author, description = book.description, image = image_path)
@@ -11,13 +11,13 @@ def create_book(db: Session, book: BookCreate, image_path: str = None):
     return new_book
 
 def get_all_books(db: Session):
-    return db.query(Book).all()
+    return db.query(Book).order_by(desc(Book.id)).all()
 
 def get_book_by_id(db: Session, id: int):
     return db.query(Book).filter(Book.id == id).first()
 
 def get_books_by_author(db: Session, author: str):
-    return db.query(Book).filter(func.lower(Book.author) == author.lower())
+    return db.query(Book).filter(func.lower(Book.author) == author.lower()).order_by(desc(Book.id))
 
 def update_book(db: Session, book_id: int, book_data: BookCreate, image_path: str = None):
     book = db.query(Book).filter(Book.id == book_id).first()
