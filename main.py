1699d6338eea32b91530b6d2ff7d7c1e9aae2b88
cristian sáez <44517223+cristianS97@users.noreply.github.com>
2026-03-02 11:28:12 -0300

    Se mejora logger

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+import time
+import sys
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+from loguru import logger
+from jose import jwt
 from models import models
 from db.database import engine
 from routers import users, books, setup, userbooks
+from helpers.auth import SECRET_KEY, ALGORITHM
+
+logger.remove()
+logger.add(
+    sys.stdout, 
+    colorize=True, 
+    format="<green>{time:HH:mm:ss}</green> | <level>{level: <8}</level> | <level>{message}</level>"
+)
 
 # fastapi dev main.py
 # Esto crea físicamente el archivo library.db y las tablas si no existen
@@ -13,6 +25,42 @@ app = FastAPI(
     description="API para practicar: Libros, Usuarios y Valoraciones",
     version="1.0.0"
 )
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    start_time = time.time()
+    
+    # 1. Intentar obtener el usuario del token (sin validar, solo para el log)
+    user_info = "Anónimo"
+    auth_header = request.headers.get("Authorization")
+
+    if auth_header and auth_header.startswith("Bearer "):
+        try:
+            token = auth_header.split(" ")[1]
+            payload = jwt.decode(
+                token, 
+                SECRET_KEY,
+                algorithms=[ALGORITHM],
+                options={"verify_signature": True, "verify_aud": False, "verify_at_hash": False, "verify_exp": True}
+            )
+            user_info = payload.get("sub", "ID Desconocido")
+        except Exception as e:
+            logger.error(f"Error en log middleware: {e}")
+            user_info = "Token-Error"
+
+    # 2. Procesar la petición
+    response = await call_next(request)
+    
+    # 3. Calcular tiempo
+    process_time = (time.time() - start_time) * 1000
+    
+    # 4. Log con el usuario incluido
+    logger.info(
+        f"USR: {user_info: <10} | {request.method: <6} {request.url.path: <20} | "
+        f"STATUS: {response.status_code} | {process_time:.2f}ms"
+    )
+    
+    return response
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
