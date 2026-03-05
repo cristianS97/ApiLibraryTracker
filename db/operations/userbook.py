b3eb36865ef80e0881f9613702f5bae8be0ae0b4
cristian sáez <44517223+cristianS97@users.noreply.github.com>
2026-03-05 15:15:44 -0300

    Se crea endpoint para obtener un registro de la librería

--- db/operations/userbook.py
+++ db/operations/userbook.py
@@ -1,5 +1,15 @@
+from sqlalchemy import func
 from sqlalchemy.orm import Session, joinedload
 from models.models import UserBook
 
 def get_library(db: Session, user_id: int):
     return db.query(UserBook).filter(UserBook.user_id == user_id).options(joinedload(UserBook.book)).all()
+
+def get_library_item(db: Session, itme_id: int):
+    data = db.query(UserBook).filter(UserBook.id == itme_id).options(joinedload(UserBook.book)).first()
+
+    if data:
+        avg_rating = db.query(func.avg(UserBook.rating)).filter(UserBook.book_id == data.book_id).scalar()
+        data.book.average_rating = avg_rating if avg_rating else 0.0
+    
+    return data
--- routers/userbooks.py
+++ routers/userbooks.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from typing import Annotated, List
 from sqlalchemy.orm import Session
 from starlette import status
 from db.database import get_db
-from db.operations.userbook import get_library
+from db.operations.userbook import get_library, get_library_item
 from models.models import User
 from models.schemas import UserBookResponse
 from helpers.auth import get_current_user
@@ -27,4 +27,22 @@ logged_user_dependency = Annotated[User, Depends(get_current_user)]
 )
 def obtener_mi_libreria(db: db_dependency, user: logged_user_dependency):
     library = get_library(db, user.id)
+    return library
+
+@router.get("/{id}/",
+    status_code=status.HTTP_200_OK,
+    summary="Obtener librería",
+    description="Se obtiene la librería del usuario logeado.",
+    responses={
+        200: {"description": "Detalle del libro"},
+        401: {"description": "No autorizado. Token inválido o expirado"},
+        404: {"description": "No se encontró el registro buscado"},
+        422: {"description": "Datos de entrada mal formados"}
+    },
+    response_model=UserBookResponse
+)
+def obtener_item_de_la_libreria(db: db_dependency, user: logged_user_dependency, id: int):
+    library = get_library_item(db, id)
+    if not library:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No se ha encontrado el elemento de la libería")
     return library
